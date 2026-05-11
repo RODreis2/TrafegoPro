7d4625ca91c3939a30b9ae49e0d8001d82a2c76e
Kelvyn <kelvyn.kaua9@gmail.com>
2026-05-11 10:48:11 -0300

    Remove unused user scaffolding

--- src/application/dto/request/user_request.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel
-
-class UserRequest(BaseModel):
-    name: str
-    email: str
-    password: str 
--- src/application/dto/response/user_response.py
+++
@@ -1,7 +0,0 @@
-from uuid import UUID 
-from pydantic import BaseModel 
-
-class UserResponse(BaseModel):
-    id: UUID
-    name: str
-    email: str
